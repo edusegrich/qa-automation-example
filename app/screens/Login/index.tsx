afc61b4c3c708ce62ca0503e16d61f0e3512d674
edustrivelabs <eduardo.s@strivelabs.io>
2023-03-30 14:30:54 +0200

    login check

--- app/screens/Login/index.tsx
+++ app/screens/Login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput, View } from 'react-native';
 import { Text, Button } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,39 +9,48 @@ import { ILoginState } from 'app/models/reducers/login';
 import NavigationService from 'app/navigation/NavigationService';
 
 interface IState {
-  loginReducer: ILoginState;
+	loginReducer: ILoginState;
 }
 
 const Login: React.FC = () => {
-  const id = useSelector((state: IState) => state.loginReducer.id);
-  const dispatch = useDispatch();
-  const onLogin = () => dispatch(loginActions.requestLogin('test', '1234'));
-  const onForgot = () => NavigationService.navigate('ForgotPassword');
-  return (
-    <View style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.login}>Login Status : {id}</Text>
-        <View style={{ margin: 20, marginBottom: 100 }}>
-          <TextInput style={styles.textInput} placeholder="email" />
-          <TextInput
-            style={styles.textInput}
-            placeholder="****"
-            secureTextEntry
-          />
-        </View>
-        <Button icon="login" mode="outlined" onPress={onLogin}>
-          Login
-        </Button>
-        <Button
-          mode="text"
-          style={styles.forgot}
-          labelStyle={styles.labelStyle}
-          onPress={onForgot}>
-          Forgot Password
-        </Button>
-      </View>
-    </View>
-  );
+	const id = useSelector((state: IState) => state.loginReducer.id);
+	const dispatch = useDispatch();
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const onChangeUsername = (text: string) => {
+		setUsername(text);
+	};
+	const onChangePassword = (text: string) => {
+		setPassword(text);
+	};
+	const onLogin = () => dispatch(loginActions.requestLogin(username, password));
+	const onForgot = () => NavigationService.navigate('ForgotPassword');
+	return (
+		<View style={styles.container}>
+			<View style={styles.container}>
+				<Text style={styles.login}>Login Status : {id}</Text>
+				<View style={{ margin: 20, marginBottom: 100 }}>
+					<TextInput style={styles.textInput} placeholder="username" onChangeText={onChangeUsername}/>
+					<TextInput
+						style={styles.textInput}
+						placeholder="****"
+						secureTextEntry
+						onChangeText={onChangePassword}
+					/>
+				</View>
+				<Button icon="login" mode="outlined" onPress={onLogin}>
+					Login
+				</Button>
+				<Button
+					mode="text"
+					style={styles.forgot}
+					labelStyle={styles.labelStyle}
+					onPress={onForgot}>
+					Forgot Password
+				</Button>
+			</View>
+		</View>
+	);
 };
 
 export default Login;
--- app/store/sagas/loginSaga.ts
+++ app/store/sagas/loginSaga.ts
@@ -10,27 +10,33 @@ import { put } from 'redux-saga/effects';
 import { Alert } from 'react-native';
 // import loginUser from 'app/services/loginUser';
 import * as loginActions from 'app/store/actions/loginActions';
+import { loginReducer } from '../reducers/loginReducer';
+import configureStore from '..';
 
 // Our worker Saga that logins the user
 export default function* loginAsync() {
-  yield put(loginActions.enableLoader());
+	yield put(loginActions.enableLoader());
 
-  //how to call api
-  //const response = yield call(loginUser, action.username, action.password);
-  //mock response
-  const response = { success: true, data: { id: 1 }, message: 'Success' };
+	//how to call api
+	//const response = yield call(loginUser, action.username, action.password);
+	//mock response
+	const { store } = configureStore();
+	let response: any;
+	const { username, password } = store.getState().loginReducer;
+	if (username === 'user' && password === '1234') response = { success: true, data: { id: 1 }, message: 'Success' };
+	else response = { data: { id: 1 }, message: 'Error en Login' };
 
-  if (response.success) {
-    yield put(loginActions.onLoginResponse(response.data));
-    yield put(loginActions.disableLoader());
+	if (response.success) {
+		yield put(loginActions.onLoginResponse(response.data));
+		yield put(loginActions.disableLoader());
 
-    // no need to call navigate as this is handled by redux store with SwitchNavigator
-    //yield call(navigationActions.navigateToHome);
-  } else {
-    yield put(loginActions.loginFailed());
-    yield put(loginActions.disableLoader());
-    setTimeout(() => {
-      Alert.alert('BoilerPlate', response.message);
-    }, 200);
-  }
+		// no need to call navigate as this is handled by redux store with SwitchNavigator
+		//yield call(navigationActions.navigateToHome);
+	} else {
+		yield put(loginActions.loginFailed());
+		yield put(loginActions.disableLoader());
+		setTimeout(() => {
+			Alert.alert('BoilerPlate', response.message);
+		}, 200);
+	}
 }
